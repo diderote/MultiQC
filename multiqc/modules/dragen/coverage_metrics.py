7031585aef6a5c17c52a3b1b8fb036dbe812e93c
Karl, Daniel <daniel.karl@astrazeneca.com>
2022-03-28 20:43:52 -0400

    fixed qc coverage parsing

--- multiqc/modules/dragen/coverage_metrics.py
+++ multiqc/modules/dragen/coverage_metrics.py
@@ -249,7 +249,7 @@ class DragenCoverageMetrics(BaseMultiqcModule):
     def add_qc_region_coverage_metrics(self):
         data_by_region_by_sample = defaultdict(dict)
         for f in self.find_log_files("dragen/qc_region_coverage_metrics"):
-            data_by_region = parse_wgs_coverage_metrics(f, r"(.*)\.qc-coverage-region-(1|2|3)_coverage_metrics.csv")
+            s_name, data_by_region = parse_wgs_coverage_metrics(f, r"(.*)\.qc-coverage-region-(1|2|3)_coverage_metrics.csv")
             for region, data in data_by_region.items():
                 if f["s_name"] in data_by_region_by_sample:
                     log.debug("Duplicate sample name found! Overwriting: {}".format(f["s_name"]))
